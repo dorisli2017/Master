10a97a6b30287f1ce2ab4352afd612d8a1f6165f
ailixiaod@gmail.com <ailixiaod@gmail.com>
2018-08-09 14:00:34 +0200

    First commit

---
+++ masterThesis/images/Experiments/impro.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo "USAGE: [filename=<name.pdf>] [logscale=yes] [column=<INT>] [title=<String>] ./cactus.sh inp1, inp2, ... | gnuplot" >&2
+
+# Remove (previous) temporary files
+rm -f *.tmp
+
+# Get the column with the plotted data, default is column 2
+if [[ -z $column ]]
+then
+    column=1
+fi
+
+# Set the plot title if specified
+if [[ -n $title ]]
+then
+		echo 'set title "'$title'"'
+fi
+
+# Set the key position and labels
+echo 'set key left'
+echo 'set xlabel "Problem"'
+echo 'set ylabel "Improvement"'
+if [[ -n $logscale ]]
+then
+    echo 'set logscale y'
+fi
+
+# Plot all the data
+first=0
+for var in "$@"
+do
+    cut -d " " -f $column $var> $var.tmp
+    if [ $first -eq 0 ]
+    then
+			echo 'plot "'$var.tmp'" w l t "'$var'"'
+			first=1
+    else
+			echo 'replot "'$var.tmp'" w l t "'$var'"'
+    fi
+done
+
+# If no filename specified just show the plot
+if [[ -z $filename ]]
+then
+    echo 'pause 1000'
+else
+    echo 'set term pdf'
+    echo 'set output "'$filename'"'
+    echo 'replot'
+fi
