ba7b92a4f39401191b72bb68318f7b3492376f5e
ailixiaod@gmail.com <ailixiaod@gmail.com>
2018-09-23 18:14:49 +0200

    update

--- masterThesis/images/DATA/K7/test.sh
+++ masterThesis/images/DATA/K7/test.sh
@@ -1,1 +1,10 @@
- filename="e1.pdf" title="K7" ./cactus.sh Random Bias Bias-Random|gnuplot
+filename="e1.pdf" title="K7" ./cactus.sh RandomInit BiasInit Bias-RandomInit|gnuplot
+filename="e2.pdf" title="K7" ./cactus.sh probSAT WALK Average Random-Flip|gnuplot
+filename="e3w.pdf" title="K7" ./cactus.sh WALK WALK-Local WALK-Global|gnuplot
+filename="e3a.pdf" title="K7" ./cactus.sh Average Average-Local Average-Global|gnuplot
+filename="e3r.pdf" title="K7" ./cactus.sh Random-Flip Random-Flip-Local Random-Flip-Global|gnuplot
+filename="e4w.pdf" title="K7" ./cactus.sh probSAT WALK.|gnuplot
+filename="e4a.pdf" title="K7" ./cactus.sh probSAT Average.|gnuplot
+filename="e4r.pdf" title="K7" ./cactus.sh probSAT Random-Flip.|gnuplot
+filename="e4.pdf" title="K7" ./cactus.sh probSAT yalSAT WALK. Average. Random-Flip.|gnuplot
+filename="e5.pdf" title="K7" ./cactus.sh probSAT yalSAT swpSolver|gnuplot
